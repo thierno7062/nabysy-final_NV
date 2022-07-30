936aa9f30a722e01329844dfaeb2633123c20387
thierno7062 <82187698+thierno7062@users.noreply.github.com>
2022-07-30 19:03:06 +0000

    3rd 30JUILLET

--- src/app/DETAIL/detail-absence/detail-absence.page.ts
+++ src/app/DETAIL/detail-absence/detail-absence.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PopupModalService } from 'src/app/services/popup-modal.service';
 
 @Component({
   selector: 'app-detail-absence',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailAbsencePage implements OnInit {
 
-  constructor() { }
+  constructor(private popupModalService: PopupModalService,) { }
 
   ngOnInit() {
+  }
+
+
+
+
+
+
+
+
+  closeModal(){
+    this.popupModalService.dismiss();
   }
 
 }
