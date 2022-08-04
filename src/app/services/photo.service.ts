35aa07872786103d8f4c9f973c1663256f08b47f
thierno7062 <82187698+thierno7062@users.noreply.github.com>
2022-08-04 16:09:50 +0000

    npm

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
