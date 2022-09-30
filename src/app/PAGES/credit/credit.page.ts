19f73de1c6548cb7a439cd3f07e7bd3d9f6f84a9
thierno7062 <82187698+thierno7062@users.noreply.github.com>
2022-09-30 23:32:14 +0000

    credit partie 2

--- src/app/PAGES/credit/credit.page.ts
+++ src/app/PAGES/credit/credit.page.ts
@@ -1,6 +1,13 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/member-ordering */
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides, MenuController } from '@ionic/angular';
+import { IonDatetime, IonSlides, MenuController } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
+import { IonicSelectableComponent } from 'ionic-selectable';
+import { LoadingService } from 'src/app/services/loading.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-credit',
@@ -8,6 +15,8 @@ import { IonSlides, MenuController } from '@ionic/angular';
   styleUrls: ['./credit.page.scss'],
 })
 export class CreditPage implements OnInit {
+  @ViewChild(IonDatetime) datetime: IonDatetime;
+  @ViewChild('selectComponent') selectComponent: IonicSelectableComponent;
 
   // Segments
   segmentList: Array<string> = ['CREDIT','HISTORIQUE DES CREDITS'];
@@ -18,7 +27,35 @@ export class CreditPage implements OnInit {
   ];
   @ViewChild('slide') slide: IonSlides;
 
-  constructor(private menu: MenuController) { }
+   /* Search Absence */
+   selected_user= null;
+   selected: any;
+   users: any;
+   listeEmploye: any;
+   toggle= true;
+
+   // Pick Date
+    selectedDate= '';
+    selectedDate2= format(new Date(),'yyyy-MM-dd');
+    modes = ['date', 'month', 'month-year','year'];
+    selectedMode= 'date';
+    showPicker = false;
+    dateValue= format(new Date(),'yyyy-MM-dd');
+    dateValue2= format(new Date(),'yyyy-MM-dd');
+    formattedString= '';
+    formattedString2= format(new Date(),'yyyy-MM-dd');
+
+    searchTerm: string;
+    nbElement: any;
+    id: any;
+
+    today= format(new Date(),'yyyy-MM-dd');
+    yesterday: any ; yesterdayToString: any; hier: any;
+    listeCredit: any;
+  constructor(private menu: MenuController,private loadingService: LoadingService,private http: HttpClient)
+   {
+    this.loadCredit();
+    }
 
   ngOnInit() {
   }
@@ -38,4 +75,98 @@ export class CreditPage implements OnInit {
     this.menu.open('menu-content');
   }
 
+   // Zone de Recherche
+   confirm(){
+    this.selectComponent.confirm();
+    this.selectComponent.close();
+    this.id=0;
+    console.log(this.selected);
+    if(this.selected){
+      this.id=this.selected.ID;
+
+    }
+    this.loadCredit();
+
+  }
+  clear(){
+    this.selectComponent.clear();
+    this.selectComponent.close();
+    this.id=0;
+    console.log(this.id);
+    this.loadCredit();
+  }
+  toggleItems(){
+    this.selectComponent.toggleItems(this.toggle);
+    this.toggle= !this.toggle;
+
+  }
+
+  // Date
+  dateChanged(value){
+    this.dateValue= value;
+    this.formattedString= format(parseISO(value),  ' MMM d, yyyy');
+    this.showPicker= false;
+    this.selectedDate=value;
+   }
+   dateChangedFin(value){
+    this.dateValue2= value;
+    this.formattedString2= format(parseISO(value),  ' MMM d, yyyy');
+    this.showPicker= false;
+    this.selectedDate2=value;
+   }
+
+   close(){
+    this.datetime.cancel(true);
+    this.loadCredit();
+   }
+   select(){
+    this.datetime.confirm(true);
+   this.loadCredit();
+  }
+
+  effacedateDebut(){
+    this.datetime.cancel(true);
+    this.selectedDate= '';
+    this.formattedString= '';
+    this.loadCredit();
+  }
+  effacedateFin(){
+    this.datetime.cancel(true);
+    this.selectedDate2= '';
+    this.formattedString2= '';
+    this.loadCredit();
+  }
+
+  loadCredit(){
+    this.loadingService.presentLoading();
+    this.readAPI(environment.endPoint+'credit_action.php?Action=LISTE_CREDIT&DATEDEBUT='+this.selectedDate+
+    '&DATEFIN='+this.selectedDate2+'&Token='+environment.tokenUser)
+    .subscribe((listes) =>{
+      this.loadingService.dismiss();
+      // console.log(Listes);
+      this.listeCredit=listes ;
+      this.nbElement=this.listeCredit.length  ;
+      console.log(this.listeCredit.length);
+      console.log(this.listeCredit);
+    });
+
+  }
+  readAPI(url: string){
+    //console.log(url);
+    return this.http.get(url);
+
+  }
+  creditDetail(credit: any){
+
+  }
+  updateCredit(credit: any){
+
+  }
+  removeCredit(credit: any){
+
+  }
+  doRefresh(event){
+    this.loadCredit();
+    event.target.complete();
+  }
 }
