71e8313064275072341f8554d14882bc4ea1af8e
paulisidore <paul_isidore@hotmail.com>
2022-10-02 03:58:23 +0000

    Ajout appInfo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   userName: '',
   passWord: '',
+  appInfo: null,
   userProfile: null,
   endPoint:'https://cloud-549579568.onetsolutions.network/nabysyrhrs/',
   employeConnecte: null,
